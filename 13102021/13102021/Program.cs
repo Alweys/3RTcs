f5f0f7d650efaf538dba6234a3dce4b09d60c3ed
BigDuck39 <81506772+BigDuck39@users.noreply.github.com>
2021-10-13 13:00:35 +0200

    Add files via upload

---
+++ 13102021/13102021/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13102021 {
+    class Program {
+
+        static void Main(string[] args) {
+            String test = "cba";
+            char[,] cArr = new char[50, 50];
+            Random random = new Random();
+
+            for(int i = 0; i < 50; i++) {
+                for(int j = 0; j < 50; j++) {
+                    cArr[i, j] = '*';//(char)('a' + random.Next(0, 26));
+
+                    
+                }
+            }
+            for(int i = 0;i<5;i++) {
+                cArr[25+i, 25] = (char)('a' + i);
+            }
+
+            Cetv.PrintArr(cArr);
+
+            Console.WriteLine(Cetv.ContainsWord(cArr,test));
+        }
+    }
+}
